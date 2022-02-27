0a4d4a5962e92ef3f848f5439165065dc9a21743
liuvaneshka <46787523+liuvaneshka@users.noreply.github.com>
2022-02-26 21:09:50 -0300

    Update ordenardiccionario.py

--- ordenardiccionario.py
+++ ordenardiccionario.py
@@ -1,13 +1,27 @@
-    lista_pedidos: list = []
+# ordenar diccionario que contiene diccionarios
+    lista: list = []
 
-    for i in pedidos:
-        pedido = pedidos[i]
-        pedido['ID pedido'] = i
-        lista_pedidos.append(pedido)
+    for clave in diccionario:
+        #tomo el subdiccionario
+        contenido = diccionario[clave]
+        #le introduzo a ese subdiccionario la clave del diccionario
+        contenido['clave'] = clave
+        # creo una lista con todos los subdiccionarios
+        lista_pedidos.append(contenido)
+        
+    # ordeno 
+    lista.sort(key=lambda p: p['clave'], reverse=False)
 
-    lista_pedidos.sort(key=lambda p: p['Año'], reverse=False)
+    lista.sort(key=lambda p: p['otra clave'], reverse=False)
 
-    lista_pedidos.sort(key=lambda p: p['Mes'], reverse=False)
-
-    for linea in lista_pedidos:
+    for linea in lista:
         print(linea)
+        
+"""
+    #ordenar diccionario con solo valores
+    import operator
+    ordenada = sorted(facturacion_periodo.items(), key=operator.itemgetter(1), reverse=True)
+
+    for linea in ordenada:
+        print(linea)
+"""
